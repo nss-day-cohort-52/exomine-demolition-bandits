d548c89fa7f17a19266a3053ece8237d6421662d
sseals1 <email@example.com>
2021-10-26 13:50:19 -0500

    craeted minerals and purchasedMinerals array in the data-store and created their modules respectively

--- scripts/database.js
+++ scripts/database.js
@@ -1,6 +1,26 @@
 const database = {
-    transientState: {}
+
+minerals: [
+    { id: 1, type: "gold", },
+    { id: 2, type: "silver", },
+    { id: 3, type: "mercury", },
+    { id: 4, type: "hermatite", },
+    { id: 5, type: "goethite", },
+    { id: 6, type: "pyrite", },
+    { id: 7, type: "galena", }, 
+    { id: 8, type: "sphalerite", }, 
+    { id: 9, type: "baryte", }, 
+    { id: 10, type: "quartz", }, 
+],
 }
+
+purchasedMinerals: [
+    {id: 1, conlonyId: 1, mineralId: 1, amountPurchased: 1 }
+]
+
+
+    transientState: {}
+
 
 export const setFacility = (facilityId) => {
     database.transientState.selectedFacility = facilityId
@@ -17,4 +37,5 @@ export const purchaseMineral = () => {
         // application can re-render and update state
         document.dispatchEvent( new CustomEvent("stateChanged") )
     }
-}
+
+
