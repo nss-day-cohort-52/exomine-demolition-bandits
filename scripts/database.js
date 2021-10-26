beba511c1653cba9dcd6719c0e45185737260279
jmason87 <joseph.mason87@gmail.com>
2021-10-26 13:43:02 -0500

    added colonies to database and added a colonies.js module

--- scripts/database.js
+++ scripts/database.js
@@ -1,20 +1,49 @@
 const database = {
-    transientState: {}
+    colonies: [
+        {
+            id: 1,
+            name: "Tatooine",
+        },
+        {
+            id: 2,
+            name: "Death Star"
+        },
+        {
+            id: 3,
+            name: "Dagobah"
+        },
+        {
+            id: 4,
+            name: "Alderaan"
+            
+        },
+        {
+            id: 5,
+            name: "Bespin"
+        },
+        {
+            id: 6,
+            name: "Endor"
+        },
+    ],
+    transientState: {},
+        
+    
 }
 
 export const setFacility = (facilityId) => {
     database.transientState.selectedFacility = facilityId
-    document.dispatchEvent( new CustomEvent("stateChanged") )
+    document.dispatchEvent(new CustomEvent("stateChanged"))
 }
 
 export const getFacilities = () => {
-    return database.facilities.map(f => ({...f}))
+    return database.facilities.map(f => ({ ...f }))
 }
 
 export const purchaseMineral = () => {
 
-        // Broadcast custom event to entire documement so that the
-        // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
-    }
+    // Broadcast custom event to entire documement so that the
+    // application can re-render and update state
+    document.dispatchEvent(new CustomEvent("stateChanged"))
 }
+
