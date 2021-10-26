a15ed87458241ca111f5a489f7d9409eda3a7708
jmason87 <joseph.mason87@gmail.com>
2021-10-26 15:57:13 -0500

    created demoBandits module

---
+++ scripts/demoBandits.js
@@ -0,0 +1,13 @@
+import {} from "./colonies.js"
+import {} from "./facilities.js"
+import {} from "./governors.js"
+import {} from "./facilityMineral.js"
+import {} from "./purchasedMinerals.js"
+import {} from "./Minerals.js"
+
+
+export const demoBandits = () => {
+    return `
+        <h1>Demo Bandits Space Shop</h1>
+    `
+}
