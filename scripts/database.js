d5830ae460c13b25287a4d1a0d816cbe4d231bb7
Haley Wilson <haleyjwilson20@gmail.com>
2021-10-26 14:02:01 -0500

    Fixed syntax errors

--- scripts/database.js
+++ scripts/database.js
@@ -6,8 +6,8 @@ const database = {
         {id: 1, name: "Hostess", status: true, colonyId: 1 },
         {id: 2, name: "Little Debbie", status: false, colonyId: 2 },
         {id: 3, name: "Sara Lee", status: true, colonyId: 3 },
-        {id: 4, name: "Entenmann's", status: false, colonyId: 1 }
-        {id: 5, name: "Mini Muffin", status: false, colonyId: 2 }
+        {id: 4, name: "Entenmann's", status: false, colonyId: 1 },
+        {id: 5, name: "Mini Muffin", status: false, colonyId: 2 },
         {id: 6, name: "Chicken Pot Pie", status: false, colonyId: 3 }
     ],
     facilityMineral: [
@@ -20,8 +20,8 @@ const database = {
         {id: 7, amount: 1500, mineralId: 7, facilityId: 1},
         {id: 8, amount: 1800, mineralId: 8, facilityId: 2},
         {id: 9, amount: 27500, mineralId: 9, facilityId: 3},
-        {id: 10, amount: 100000, mineralId: 10, facilityId: 4}
-        {id: 11, amount: 22000, mineralId: 11, facilityId: 5}
+        {id: 10, amount: 100000, mineralId: 10, facilityId: 4},
+        {id: 11, amount: 22000, mineralId: 11, facilityId: 5},
         {id: 12, amount: 500, mineralId: 12, facilityId: 6}
     ],
 }
