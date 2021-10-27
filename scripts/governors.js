48f352e892231aa55d20b60d3bc3317dea79e28c
Haley Wilson <haleyjwilson20@gmail.com>
2021-10-27 11:28:08 -0500

    Lots of changes

--- scripts/governors.js
+++ scripts/governors.js
@@ -1,4 +1,27 @@
 // imports the array governors from the database
-import  {getGovernors} from "./database.js"
+import { getGovernors, setGovernors } from "./database.js"
 // State variable (copy of the data)
 const governors = getGovernors()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "governor") {
+            setGovernors(parseInt(event.target.value))
+        }
+    }
+)
+
+export const governorChoices = () => {
+    let html = "<h2>Governors</h2>"
+
+    html += `<select id="governors">`
+    html += `<option value="0">Select A Governor...</option>`
+
+    for (const governor of governors) {
+        html += `<option value="${governor.id}">${governor.name}</option>`
+    }
+    html += "</select>"
+
+    return html
+}
