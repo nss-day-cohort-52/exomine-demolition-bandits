f522d29ae7aea67d2f6ebb53ab18899a229b5d3a
sseals1 <email@example.com>
2021-10-28 13:52:26 -0500

    Imported the getMinerals function on PurchaseMineral module. Created the Purchase Mineral button in the demoBandits module and added the click Event Listener for the Purchase Mineral button that will be displayed in the DOM. Changed headers on the DOM for Governor and Facility. Created and export function whose responsibility will be to get the current state of the mineral choices made in facility mineral and render those choices in the DOM above the Purchase Mineral button

--- scripts/purchasedMinerals.js
+++ scripts/purchasedMinerals.js
@@ -1,23 +1,24 @@
-import { getMinerals } from "./database.js"
+import { getMinerals, getTransState } from "./database.js"
 
-document.addEventListener(
-    "click",
-    (changeEvent) => {
-        if (changeEvent.target.id === "Purchase Mineral") {
+document.addEventListener(  // event listener that listens on DOM
+    "click", // event type is click for when the user clicks on the Purchase Mineral button
+    (changeEvent) => { // function parameter whose valur is the result of the if statement
+        if (changeEvent.target.id === "Purchase Mineral") { // comparision statement that checks if the clicked item is equal to the straing "Purchase Mineral"
             
             
         }
     }
 )
-const minerals = getMinerals()
+const minerals = getMinerals() // storing the result of the getMinerals function, which is an Array in the minerals variable
 
-export const mineralSelection = () => {
-    let html = "<h2>Space Cart</h2>"
+export const mineralSelection = () => { // defining an export function that will get the state of the mineral choices 
+    const state = getTransState()
+    let html = "<h2>Space Cart</h2>" // Title that will be displayed above the Purchase Mineral button
 
 
 
-    for (const mineral of minerals) {
-        html += `<option value="${governor.id}">${governor.name}</option>`
+    for (const stateObj of state) { // iterate through the transientState array
+        html += `<option value="${stateObj.id}">${minerals.name}</option>`
     }
     html += "</select>"
 
