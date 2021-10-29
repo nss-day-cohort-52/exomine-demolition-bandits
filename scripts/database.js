bf7634df7b2082ae985a78a2c13ba40bdf0a51d1
Haley Wilson <haleyjwilson20@gmail.com>
2021-10-29 15:48:40 -0500

    Final commit

--- scripts/database.js
+++ scripts/database.js
@@ -10,18 +10,18 @@ const database = {
         { id: 6, name: "Chicken Pot Pie", status: false, colonyId: 3 }
     ],
     facilityMinerals: [
-        { id: 1, amount: 10000, mineralId: 1, facilityId: 1 },
-        { id: 2, amount: 12000, mineralId: 2, facilityId: 2 },
-        { id: 3, amount: 8000, mineralId: 3, facilityId: 3 },
-        { id: 4, amount: 15000, mineralId: 4, facilityId: 4 },
-        { id: 5, amount: 13000, mineralId: 5, facilityId: 5 },
-        { id: 6, amount: 5000, mineralId: 6, facilityId: 6 },
-        { id: 7, amount: 1500, mineralId: 7, facilityId: 1 },
-        { id: 8, amount: 1800, mineralId: 8, facilityId: 2 },
-        { id: 9, amount: 27500, mineralId: 9, facilityId: 3 },
-        { id: 10, amount: 100000, mineralId: 10, facilityId: 4 },
-        { id: 11, amount: 22000, mineralId: 6, facilityId: 5 },
-        { id: 12, amount: 500, mineralId: 8, facilityId: 6 }
+        { id: 1, mineralId: 1, facilityId: 1 },
+        { id: 2, mineralId: 2, facilityId: 2 },
+        { id: 3, mineralId: 3, facilityId: 3 },
+        { id: 4, mineralId: 4, facilityId: 4 },
+        { id: 5, mineralId: 5, facilityId: 5 },
+        { id: 6, mineralId: 6, facilityId: 6 },
+        { id: 7, mineralId: 7, facilityId: 1 },
+        { id: 8, mineralId: 8, facilityId: 2 },
+        { id: 9, mineralId: 9, facilityId: 3 },
+        { id: 10, mineralId: 10, facilityId: 4 },
+        { id: 11, mineralId: 6, facilityId: 5 },
+        { id: 12, mineralId: 8, facilityId: 6 }
     ],
     governors: [
         { id: 1, name: "Ding-Dong", colonyId: 1, },
@@ -38,25 +38,25 @@ const database = {
     ],
     minerals: [
         { id: 1, type: "Gold", amount: 10000 },
-        { id: 2, type: "Silver", amount: 10000 },
-        { id: 3, type: "Mercury", amount: 10000 },
-        { id: 4, type: "Hermatite", amount: 10000 },
-        { id: 5, type: "Goethite", amount: 10000 },
-        { id: 6, type: "Pyrite", amount: 10000 },
-        { id: 7, type: "Galena", amount: 10000 },
-        { id: 8, type: "Sphalerite", amount: 10000 },
-        { id: 9, type: "Baryte", amount: 10000 },
-        { id: 10, type: "Quartz", amount: 10000 }
+        { id: 2, type: "Silver", amount: 12000 },
+        { id: 3, type: "Mercury", amount: 8000 },
+        { id: 4, type: "Hermatite", amount: 15000 },
+        { id: 5, type: "Goethite", amount: 13000 },
+        { id: 6, type: "Pyrite", amount: 5000 },
+        { id: 7, type: "Galena", amount: 1500 },
+        { id: 8, type: "Sphalerite", amount: 1800 },
+        { id: 9, type: "Baryte", amount: 27500 },
+        { id: 10, type: "Quartz", amount: 22000 }
     ],
     purchasedMinerals: [
         { id: 1, colonyId: 1, mineralId: 1, facilityId: 1, governorId: 2, amountPurchased: 1 }
     ],
-    transientState: { },
+    transientState: {},
 }
 
 export const addPurchasedMinerals = () => {
     // Copy the current state of user choices
-    const newOrder = {...database.transientState}
+    const newOrder = { ...database.transientState }
 
     // Add a new primary key to the object
     const lastIndex = database.purchasedMinerals.length - 1
@@ -75,6 +75,7 @@ export const addPurchasedMinerals = () => {
     // Broadcast a notification that permanent state has changed
     document.dispatchEvent(new CustomEvent("stateChanged"))
 }
+
 export const getFacilities = () => {
     return database.facilities.map(facility => ({ ...facility }))
 }
@@ -106,17 +107,12 @@ export const getTransState = () => {
 export const setFacilities = (id) => {
     database.transientState.facilityId = id
     document.dispatchEvent(new CustomEvent("stateChanged"))
-    
 }
-
-
 
 export const setGovernors = (id) => {
     database.transientState.governorId = id
     document.dispatchEvent(new CustomEvent("stateChanged"))
 }
-
-
 
 export const setMinerals = (id) => {
     database.transientState.mineralId = id
@@ -127,8 +123,8 @@ export const setColonies = (id) => {
     database.transientState.colonyId = id
 }
 
-export const setFacilityMinerals = (facilityId) => {
-    database.transientState.selectedFacility = facilityId
+export const setFacilityMinerals = (id) => {
+    database.transientState.selectedFacility = id
     document.dispatchEvent(new CustomEvent("stateChanged"))
 }
 
--- scripts/demoBandits.js
+++ scripts/demoBandits.js
@@ -8,6 +8,7 @@ import {  } from "./purchasedMinerals.js"
 
 import { chooseMinerals } from "./Minerals.js"
 import { mineralSelection } from "./purchasedMinerals.js"
+import { addPurchasedMinerals } from "./database.js"
 
 
 // document.addEventListener(
@@ -20,14 +21,11 @@ import { mineralSelection } from "./purchasedMinerals.js"
 // )
 
 document.addEventListener(
-    "change",
+    "click",
     (event) => {
-        if (event.target.id === "facilities") {
-            chooseFMinerals()
-        }
-    }
-)
-
+        if (event.target.id === "orderButton") {
+            addPurchasedMinerals()}
+    })
 
 export const demoBandits = () => {
     return `
@@ -44,18 +42,12 @@ export const demoBandits = () => {
                 ${facilityChoices()}
             </section>
         </article>
-
-       
-
-
     
-
         <article class="radioMinerals">
             <section id="mineral">
                 <h2>Facility Minerals</h2>
                 ${chooseFMinerals()}
             </section>
-
         </article></fieldset>
 
         <div><fieldset class="fieldset-set-right">
@@ -70,7 +62,7 @@ export const demoBandits = () => {
             <section>
                 <h2>Space Cart</h2>
                     ${mineralSelection()}
-                <button>Purchase Minerals</button>
+                <button id="orderButton">Purchase Minerals</button>
             </section>
         </article></fieldset></div>
 
